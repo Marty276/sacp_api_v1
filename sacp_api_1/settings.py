8aa455427306f2f17993125423b61ae47acf8ca8
Marty_276 <martybedoya10@gmail.com>
2023-09-08 13:28:58 -0400

    cors headers updated

--- sacp_api_1/settings.py
+++ sacp_api_1/settings.py
@@ -146,6 +146,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #CORS SETTINGS
 
-CORS_ALLOWED_ORIGINS = [
-    
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
 ]
